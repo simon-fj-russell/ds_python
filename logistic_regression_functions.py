9db22f27f6f892676ea6c1be6d79fad3e24eeb0d
simon-fj-russell <54851504+simon-fj-russell@users.noreply.github.com>
2019-10-15 11:15:55 +0200

    Update logistic_regression_functions.py

--- logistic_regression_functions.py
+++ logistic_regression_functions.py
@@ -11,7 +11,7 @@ df = pd.read_csv('titanic.csv')
 df_raw = pd.read_csv('titanic.csv')
 
 # Look at dataframe, column titles and missing data.
-# df.describe() also give a very detailed breakdown of the data in the column
+# df.describe() also give a very detailed breakdown of the data in the column.
 print(df.info())
 
 # Age has null values, fill in the missing data with the median age.
@@ -31,21 +31,21 @@ print(df.info())
 correlation = df.corr().loc[['Pclass', 'Age', 'is_male', 'SibSp', 'Fare', 'Parch'], ['Survived']]
 print(correlation)
 
-# Select the model we want to use
+# Select the model we want to use.
 logreg = LogisticRegression(solver='lbfgs')
 
 # Define the data we are going to feed into it. Just selecting the columns we want from the dataframe
-# 'Feature' Columns
+# 'Feature' Columns.
 X = df[['Pclass', 'Age', 'is_male', 'SibSp', 'Fare', 'Parch']]
-# Instead of selecting the columns we want, we can drop columns. Using axis=1 to selects columns not rows
+# Instead of selecting the columns we want, we can drop columns. Using axis=1 to selects columns not rows.
 # X = df.drop('Survived', axis=1)
 # 'Target' Column.
 y = df[['Survived']]
 
-# Split the data into a train and test set. 80% train, 20% test with a random seed of 42
+# Split the data into a train and test set. 80% train, 20% test with a random seed of 42.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-# Fitting the model
+# Fitting the model.
 logreg.fit(X_train, y_train)
 
 # Now check the accuracy of the model using the test data.
