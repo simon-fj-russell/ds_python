4ea897aac65db43f66c21e97b49de8b411c905ff
simon-fj-russell <54851504+simon-fj-russell@users.noreply.github.com>
2019-10-15 10:52:05 +0200

    Update logistic_regression_functions.py

--- logistic_regression_functions.py
+++ logistic_regression_functions.py
@@ -1,75 +1,53 @@
-# A function that does the Logistic Regression for you.
-# Import needed functions
-from sklearn import linear_model
-from sklearn.metrics import roc_auc_score
+# Linear Regression model using stats models
+# First import the modules
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-# Takes the data Frame (basetable)
-# The predictors you want it to work on as a list.
-# (target) is the 'y' column name, the one you want it to predict as a list.
-# (print_coef) True/False print out the coefficients of the predictors.
-# (print_acu) True/False print out the acu of the model.
-def logistic_regression(basetable, predictors, target, print_coef=False, print_acu=False):
+# Read in Titanic passenger data as a Pandas DataFrame.
+df = pd.read_csv('titanic.csv')
 
-    # Create the logistic regression model
-    logreg = linear_model.LogisticRegression()
+# Also make a raw data that we won't change.
+df_raw = pd.read_csv('titanic.csv')
 
-    # Define x & y then split the data into train & test groups.
-    x = basetable[predictors]
-    y = basetable[target]
+# Look at dataframe, column titles and missing data.
+# df.describe() also give a very detailed breakdown of the data in the column
+print(df.info())
 
-    # Fit the model.
-    logreg.fit(x, y)
+# Age has null values, fill in the missing data with the median age.
+df['Age'] = df['Age'].fillna(df['Age'].median())
 
-    # Print out the coefficients and intercept if print_coef is True.
-    if print_coef is True:
-        coef = logreg.coef_
-        intercept = logreg.intercept_
-        for p, c in zip(predictors, list(coef[0])):
-            print(p + '\t' + str(c))
-        print(intercept)
+# Change 'Sex' to an int and rename the column.
+df['Sex'] = df['Sex'].replace(['male', 'female'], [1, 0])
+df = df.rename(columns={'Sex': 'is_male'})
 
-    # Do prediction on test data.
-    # predictions gives 2 numbers for each user, the first that the target is 0 the second that the target is 1
-    predictions = logreg.predict_proba(x)[:, 1]
+# Check that it worked.
+print(df.info())
 
-    # Check accuracy and print it out.
-    auc = roc_auc_score(y, predictions)
-    if print_acu is True:
-        print("AUC of the model: " + str(round(auc, 2)))
+# Or you can use an apply lambda function (like an if statement).
+# df['Sex'] = df['Sex'].apply(lambda x: 1 if x == 'male' else 0)
 
-    return auc
+# Check the correlation between the “feature” columns and the “Target” column.
+correlation = df.corr().loc[['Pclass', 'Age', 'is_male', 'SibSp', 'Fare', 'Parch'], ['Survived']]
+print(correlation)
 
+# Select the model we want to use
+logreg = LogisticRegression(solver='lbfgs')
 
-# A function that goes though the predictors to find the best one.
-# The current_predictors are the predictors you already have.
-# candidate_predictors is a list of other predictors you want to try.
-# (target) is the 'y' column name, the one you want it to predict as a string.
-# Takes the data Frame (basetable)
-# The function will return the best one from the list of candidate_predictors.
-def next_best(current_predictors, candidate_predictors, target, basetable):
-    # Initialise the best auc and variable.
-    best_auc = -1
-    best_predictors = None
+# Define the data we are going to feed into it. Just selecting the columns we want from the dataframe
+# 'Feature' Columns
+X = df[['Pclass', 'Age', 'is_male', 'SibSp', 'Fare', 'Parch']]
+# Instead of selecting the columns we want, we can drop columns. Using axis=1 to selects columns not rows
+# X = df.drop('Survived', axis=1)
+# 'Target' Column.
+y = df[['Survived']]
 
-    for v in candidate_predictors:
-        auc_v = logistic_regression(basetable, current_predictors + [v], target)
+# Split the data into a train and test set. 80% train, 20% test with a random seed of 42
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-        if auc_v >= best_auc:
-            best_auc = auc_v
-            best_predictors = v
+# Fitting the model
+logreg.fit(X_train, y_train)
 
-    return (best_predictors)
-
-# To iterate over all possible predictors:
-# Set the current_predictors as an empty list
-# And the candidate_predictors as a list of all possible predictors.
-# Set the max_number_predictors that you want (default is 5).
-def find_candidate_predictors(basetable, current_predictors, candidate_predictors, target, max_number_predictors=5):
-    number_iterations = min(max_number_predictors, len(candidate_predictors))
-    for i in range(0, number_iterations):
-        next_cand = next_best(current_predictors, candidate_predictors, target, basetable)
-
-        current_predictors = current_predictors + [next_cand]
-        candidate_predictors.remove(next_cand)
-
-    return current_predictors
+# Now check the accuracy of the model using the test data.
+ml_score = logreg.score(X_test, y_test)
+print("Accuracy of model =", round(ml_score, 4))
